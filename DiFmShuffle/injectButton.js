986a38b90eeb5d059eae38aea986f079903ccd04
Hebron George <hebrontgeorge@gmail.com>
2017-11-19 16:14:20 -0600

    WIP - need to get list of channel ids from correct context

--- DiFmShuffle/injectButton.js
+++ DiFmShuffle/injectButton.js
@@ -1,19 +1,23 @@
+inject_shuffle_button();
+
 function randChan() {
-   if (typeof AudioAddict != "undefined"){
-         var channels = AudioAddict.WP.channels;
+     chrome.storage.sync.get('channel_ids', function(channels) {
+     	console.log("List of channels: " + JSON.stringify(channels));
+     	console.log("Number of channels: " + channels['channel_ids'].length);
          channel = channels[Math.floor(Math.random()*channels.length)];
          di.app.WebplayerApp.app.commands.execute("webplayer:play:channel", channel.id);
-   } else {
-      console.log("AudioAddict is not defined!");
-   }
+     });
 }
 
-var channel;
-var shuffleButtonLI = document.createElement("li");
-shuffleButtonLI.className += "menu-item";
-var shuffleButtonA = document.createElement("a");
-shuffleButtonA.innerHTML = '<i class=""></i><span><span class=":before"></span>Shuffle</span>';
-shuffleButtonLI.onclick = function () {randChan();};
-shuffleButtonLI.appendChild(shuffleButtonA);
-var nav = document.getElementById('side-nav').getElementsByTagName('ul')[0];
-nav.appendChild(shuffleButtonLI);
+function inject_shuffle_button() {
+	var channel;
+	var shuffleButtonLI = document.createElement("li");
+	shuffleButtonLI.className += "menu-item";
+	var shuffleButtonA = document.createElement("a");
+	shuffleButtonA.innerHTML = '<i class=""></i><span><span class=":before"></span>Shuffle</span>';
+	shuffleButtonLI.onclick = function () {randChan();};
+	shuffleButtonLI.appendChild(shuffleButtonA);
+	var nav = document.getElementById('side-nav').getElementsByTagName('ul')[0];
+	nav.appendChild(shuffleButtonLI);
+}
+
--- DiFmShuffle/myscript.js
+++ DiFmShuffle/myscript.js
@@ -1,6 +1,33 @@
-var script = document.createElement('script');
-script.setAttribute("type", "text/javascript");
-script.setAttribute("async", true);
-script.setAttribute("src", chrome.extension.getURL("injectButton.js")); //Assuming your host supports both http and https
-var head = document.head || document.getElementsByTagName( "head" )[0] || document.documentElement;
-head.insertBefore(script, head.firstChild)
+insert_shuffle_button();
+get_list_of_channels();
+
+function insert_shuffle_button() {
+	var script = document.createElement('script');
+	script.setAttribute("type", "text/javascript");
+	script.setAttribute("async", true);
+	script.setAttribute("src", chrome.extension.getURL("injectButton.js")); //Assuming your host supports both http and https
+	var head = document.head || document.getElementsByTagName( "head" )[0] || document.documentElement;
+	head.insertBefore(script, head.firstChild)
+}
+
+function get_list_of_channels() {
+	var audio_addict_api = 'https://api.audioaddict.com/v1/di/channels.json';
+	$.getJSON(audio_addict_api, function(data, status){
+		if (status == "success"){
+			var channel_ids = get_channel_ids_from_raw_list(data);		
+			store_channel_ids(channel_ids);	
+		}
+	})
+}
+
+function get_channel_ids_from_raw_list(channels) {
+	return channels.map(function(channel){
+		return channel.id
+	});
+}
+
+function store_channel_ids(ids_to_store) {
+	chrome.storage.sync.set({'channel_ids': ids_to_store}, function() {
+		console.log('Channel list updated -- DiFmShuffle -- d[^_^]b');
+	})
+}
